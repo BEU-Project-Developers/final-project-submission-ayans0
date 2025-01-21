90c39b9a2761b915f230f8da625382e3d3362cc4
Ayan <ayanns501@gmail.com>
2025-01-21 20:57:14 +0400

    Room

--- HotelManagement/UserControl/RoomControl.cs
+++ HotelManagement/UserControl/RoomControl.cs
@@ -7,15 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AMRConnector;
 
 namespace HotelManagement.UserControl
 {
     public partial class RoomControl : System.Windows.Forms.UserControl
     {
+        DbConnector db;
+        private string No = "", Free = "";
         public RoomControl()
         {
             InitializeComponent();
+            db=new DbConnector();
         }
+        public void Clear()
+        {
+            comboBoxType.SelectedIndex = -1;
+            textBoxPhoneNo.Clear();
+            radioButtonNo.Checked = false;
+            radioButtonYes.Checked = false;
+            tabControlRoom.SelectedTab = tabPageAddRoom;
+        }
+        public void Clear1()
+        {
+            comboBoxType1.SelectedIndex = -1;
+            textBoxPhoneNo1.Clear();
+            radioButtonNo1.Checked = false;
+            radioButtonYes1.Checked = false;
+            No = "";
+        }
+
 
         private void InitializeComponent()
         {
@@ -89,6 +110,7 @@ namespace HotelManagement.UserControl
             this.tabPageAddRoom.TabIndex = 0;
             this.tabPageAddRoom.Text = "Add Room";
             this.tabPageAddRoom.Click += new System.EventHandler(this.tabPageAddRoom_Click);
+            this.tabPageAddRoom.Leave += new System.EventHandler(this.tabPageAddRoom_Leave);
             // 
             // radioButtonNo
             // 
@@ -117,7 +139,7 @@ namespace HotelManagement.UserControl
             this.label4.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(122, 281);
+            this.label4.Location = new System.Drawing.Point(122, 276);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(81, 34);
             this.label4.TabIndex = 13;
@@ -135,7 +157,7 @@ namespace HotelManagement.UserControl
             "Double",
             "Family",
             "Suite"});
-            this.comboBoxType.Location = new System.Drawing.Point(128, 220);
+            this.comboBoxType.Location = new System.Drawing.Point(128, 215);
             this.comboBoxType.Name = "comboBoxType";
             this.comboBoxType.Size = new System.Drawing.Size(449, 42);
             this.comboBoxType.TabIndex = 12;
@@ -147,7 +169,7 @@ namespace HotelManagement.UserControl
             this.buttonAdd.FlatAppearance.BorderSize = 0;
             this.buttonAdd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonAdd.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonAdd.Location = new System.Drawing.Point(128, 385);
+            this.buttonAdd.Location = new System.Drawing.Point(128, 380);
             this.buttonAdd.Name = "buttonAdd";
             this.buttonAdd.Size = new System.Drawing.Size(205, 47);
             this.buttonAdd.TabIndex = 11;
@@ -158,7 +180,7 @@ namespace HotelManagement.UserControl
             // textBoxPhoneNo
             // 
             this.textBoxPhoneNo.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBoxPhoneNo.Location = new System.Drawing.Point(739, 220);
+            this.textBoxPhoneNo.Location = new System.Drawing.Point(739, 215);
             this.textBoxPhoneNo.Name = "textBoxPhoneNo";
             this.textBoxPhoneNo.Size = new System.Drawing.Size(449, 42);
             this.textBoxPhoneNo.TabIndex = 10;
@@ -168,7 +190,7 @@ namespace HotelManagement.UserControl
             this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(733, 174);
+            this.label3.Location = new System.Drawing.Point(733, 169);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(226, 34);
             this.label3.TabIndex = 9;
@@ -179,7 +201,7 @@ namespace HotelManagement.UserControl
             this.label2.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(122, 174);
+            this.label2.Location = new System.Drawing.Point(122, 169);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(87, 34);
             this.label2.TabIndex = 7;
@@ -191,11 +213,12 @@ namespace HotelManagement.UserControl
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold);
             this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(213)))), ((int)(((byte)(143)))));
-            this.label1.Location = new System.Drawing.Point(121, 78);
+            this.label1.Location = new System.Drawing.Point(121, 73);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(195, 39);
             this.label1.TabIndex = 6;
             this.label1.Text = "Add Room:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // tabPageSearchRoom
             // 
@@ -210,6 +233,8 @@ namespace HotelManagement.UserControl
             this.tabPageSearchRoom.Size = new System.Drawing.Size(1287, 613);
             this.tabPageSearchRoom.TabIndex = 1;
             this.tabPageSearchRoom.Text = "Search Room";
+            this.tabPageSearchRoom.Enter += new System.EventHandler(this.tabPageSearchRoom_Enter);
+            this.tabPageSearchRoom.Leave += new System.EventHandler(this.tabPageSearchRoom_Leave);
             // 
             // dataGridViewRoom
             // 
@@ -224,13 +249,14 @@ namespace HotelManagement.UserControl
             this.Column2,
             this.Column3,
             this.Column4});
-            this.dataGridViewRoom.Location = new System.Drawing.Point(123, 216);
+            this.dataGridViewRoom.Location = new System.Drawing.Point(123, 211);
             this.dataGridViewRoom.Name = "dataGridViewRoom";
             this.dataGridViewRoom.ReadOnly = true;
             this.dataGridViewRoom.RowHeadersWidth = 72;
             this.dataGridViewRoom.RowTemplate.Height = 31;
             this.dataGridViewRoom.Size = new System.Drawing.Size(1111, 330);
             this.dataGridViewRoom.TabIndex = 13;
+            this.dataGridViewRoom.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewRoom_CellClick);
             // 
             // Column1
             // 
@@ -267,17 +293,18 @@ namespace HotelManagement.UserControl
             // textBoxSearchRoomNo
             // 
             this.textBoxSearchRoomNo.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBoxSearchRoomNo.Location = new System.Drawing.Point(393, 151);
+            this.textBoxSearchRoomNo.Location = new System.Drawing.Point(393, 146);
             this.textBoxSearchRoomNo.Name = "textBoxSearchRoomNo";
             this.textBoxSearchRoomNo.Size = new System.Drawing.Size(449, 42);
             this.textBoxSearchRoomNo.TabIndex = 12;
+            this.textBoxSearchRoomNo.TextChanged += new System.EventHandler(this.textBoxSearchRoomNo_TextChanged);
             // 
             // label6
             // 
             this.label6.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(387, 114);
+            this.label6.Location = new System.Drawing.Point(387, 109);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(214, 34);
             this.label6.TabIndex = 11;
@@ -289,7 +316,7 @@ namespace HotelManagement.UserControl
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(213)))), ((int)(((byte)(143)))));
-            this.label7.Location = new System.Drawing.Point(117, 57);
+            this.label7.Location = new System.Drawing.Point(117, 52);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(204, 34);
             this.label7.TabIndex = 10;
@@ -322,12 +349,13 @@ namespace HotelManagement.UserControl
             this.buttonDelete.FlatAppearance.BorderSize = 0;
             this.buttonDelete.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonDelete.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonDelete.Location = new System.Drawing.Point(378, 419);
+            this.buttonDelete.Location = new System.Drawing.Point(378, 414);
             this.buttonDelete.Name = "buttonDelete";
             this.buttonDelete.Size = new System.Drawing.Size(205, 47);
             this.buttonDelete.TabIndex = 25;
             this.buttonDelete.Text = "Delete";
             this.buttonDelete.UseVisualStyleBackColor = false;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
             // 
             // radioButtonNo1
             // 
@@ -356,7 +384,7 @@ namespace HotelManagement.UserControl
             this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(143, 315);
+            this.label5.Location = new System.Drawing.Point(143, 310);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(81, 34);
             this.label5.TabIndex = 22;
@@ -374,7 +402,7 @@ namespace HotelManagement.UserControl
             "Double",
             "Family",
             "Suite"});
-            this.comboBoxType1.Location = new System.Drawing.Point(149, 254);
+            this.comboBoxType1.Location = new System.Drawing.Point(149, 249);
             this.comboBoxType1.Name = "comboBoxType1";
             this.comboBoxType1.Size = new System.Drawing.Size(449, 42);
             this.comboBoxType1.TabIndex = 21;
@@ -386,17 +414,18 @@ namespace HotelManagement.UserControl
             this.buttonUpdate.FlatAppearance.BorderSize = 0;
             this.buttonUpdate.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.buttonUpdate.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonUpdate.Location = new System.Drawing.Point(149, 419);
+            this.buttonUpdate.Location = new System.Drawing.Point(149, 414);
             this.buttonUpdate.Name = "buttonUpdate";
             this.buttonUpdate.Size = new System.Drawing.Size(205, 47);
             this.buttonUpdate.TabIndex = 20;
             this.buttonUpdate.Text = "Update";
             this.buttonUpdate.UseVisualStyleBackColor = false;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
             // 
             // textBoxPhoneNo1
             // 
             this.textBoxPhoneNo1.Anchor = System.Windows.Forms.AnchorStyles.None;
-            this.textBoxPhoneNo1.Location = new System.Drawing.Point(760, 254);
+            this.textBoxPhoneNo1.Location = new System.Drawing.Point(760, 249);
             this.textBoxPhoneNo1.Name = "textBoxPhoneNo1";
             this.textBoxPhoneNo1.Size = new System.Drawing.Size(449, 42);
             this.textBoxPhoneNo1.TabIndex = 19;
@@ -406,7 +435,7 @@ namespace HotelManagement.UserControl
             this.label8.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.Location = new System.Drawing.Point(754, 208);
+            this.label8.Location = new System.Drawing.Point(754, 203);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(226, 34);
             this.label8.TabIndex = 18;
@@ -417,7 +446,7 @@ namespace HotelManagement.UserControl
             this.label9.Anchor = System.Windows.Forms.AnchorStyles.None;
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label9.Location = new System.Drawing.Point(143, 208);
+            this.label9.Location = new System.Drawing.Point(143, 203);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(87, 34);
             this.label9.TabIndex = 17;
@@ -429,7 +458,7 @@ namespace HotelManagement.UserControl
             this.label10.AutoSize = true;
             this.label10.Font = new System.Drawing.Font("Century Gothic", 14F, System.Drawing.FontStyle.Bold);
             this.label10.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(213)))), ((int)(((byte)(143)))));
-            this.label10.Location = new System.Drawing.Point(142, 112);
+            this.label10.Location = new System.Drawing.Point(142, 107);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(431, 39);
             this.label10.TabIndex = 16;
@@ -456,10 +485,131 @@ namespace HotelManagement.UserControl
 
         private void buttonAdd_Click(object sender, EventArgs e)
         {
-
+            if (radioButtonYes.Checked) Free = "Yes";
+            if (radioButtonNo.Checked) Free = "No";
+            bool check;
+            if (comboBoxType.SelectedIndex==-1 || textBoxPhoneNo.Text.Trim()==string.Empty 
+                || Free=="")
+            {
+                MessageBox.Show("Please fill out all fields.", "Require all fields",
+                    MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                check = db.AddRoom(comboBoxType.SelectedItem.ToString(),
+                    textBoxPhoneNo.Text.Trim(),Free);
+                if (check)
+                {
+                    Clear();
+                }
+            }
         }
 
         private void tabPageAddRoom_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPageAddRoom_Leave(object sender, EventArgs e)
+        {
+            Clear();
+            Clear1();
+        }
+
+        private void tabPageSearchRoom_Enter(object sender, EventArgs e)
+        {
+            db.DisplayAndSearch("SELECT * FROM Room_Table", dataGridViewRoom);
+        }
+
+        private void tabPageSearchRoom_Leave(object sender, EventArgs e)
+        {
+            textBoxSearchRoomNo.Clear();
+        }
+
+        private void textBoxSearchRoomNo_TextChanged(object sender, EventArgs e)
+        {
+            db.DisplayAndSearch("SELECT * FROM Room_Table WHERE Room_Number LIKE '%" +
+                textBoxSearchRoomNo.Text + "%'", dataGridViewRoom); 
+        }
+
+        private void dataGridViewRoom_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if(e.RowIndex!=-1)
+            {
+                DataGridViewRow row=dataGridViewRoom.Rows[e.RowIndex];
+                No=row.Cells[0].Value.ToString();
+                comboBoxType1.SelectedItem=row.Cells[1].Value.ToString();
+                textBoxPhoneNo1.Text=row.Cells[2].Value.ToString();
+                Free=row.Cells[3].Value.ToString();
+                if (Free == "Yes") radioButtonYes1.Checked = true;
+                if(Free =="No") radioButtonNo1.Checked = true;
+            }
+        }
+
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (radioButtonYes1.Checked) Free = "Yes";
+            if (radioButtonNo1.Checked) Free = "No";
+            bool check;
+            if (No != "")
+            {
+                if (comboBoxType1.SelectedIndex == -1 || textBoxPhoneNo1.Text.Trim() == string.Empty
+                || Free == "")
+                {
+                    MessageBox.Show("Please fill out all fields.", "Require all fields",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information); }
+                else
+                {
+                    check = db.UpdateRoom(No, comboBoxType1.SelectedItem.ToString(),
+                        textBoxPhoneNo1.Text.Trim(), Free);
+                    if (check)
+                    {
+                        Clear1();
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please first select row from table.",
+                    "Seelection of row", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (radioButtonYes1.Checked) Free = "Yes";
+            if (radioButtonNo1.Checked) Free = "No";
+            bool check;
+            if (No != "")
+            {
+                if (comboBoxType1.SelectedIndex == -1 || textBoxPhoneNo1.Text.Trim() == string.Empty
+                || Free == "")
+                {
+                    MessageBox.Show("Please fill out all fields.", "Require all fields",
+                        MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    DialogResult result = MessageBox.Show("Are you want to delete this room?",
+                         "Room delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (DialogResult.Yes == result)
+                    {
+                        check = db.DeleteRoom(No);
+                        if (check)
+                        {
+                            Clear1();
+                        }
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please first select row from table.",
+                    "Seelection of row", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
